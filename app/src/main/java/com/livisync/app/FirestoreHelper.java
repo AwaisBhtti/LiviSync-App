2d4c044dc4b14306d2dacedae94e69ce01e0279a
awais <awaisakhtar205@gmail.com>
2026-05-02 20:27:02 +0500

    debugged firebase issues

--- app/src/main/java/com/livisync/app/FirestoreHelper.java
+++ app/src/main/java/com/livisync/app/FirestoreHelper.java
@@ -1,5 +1,7 @@
 package com.livisync.app;
 
+import android.util.Log;
+
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -7,21 +9,30 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 public class FirestoreHelper {
 
+    private static final String TAG = "FirestoreHelper";
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public static void getUserProfile(String uid, OnSuccessListener<DocumentSnapshot> listener) {
-        db.collection("users").document(uid).get().addOnSuccessListener(listener);
+        db.collection("users").document(uid).get()
+                .addOnSuccessListener(listener)
+                .addOnFailureListener(e -> Log.e(TAG, "Error getting user profile", e));
     }
 
     public static void getPreferences(String uid, OnSuccessListener<DocumentSnapshot> listener) {
-        db.collection("preferences").document(uid).get().addOnSuccessListener(listener);
+        db.collection("preferences").document(uid).get()
+                .addOnSuccessListener(listener)
+                .addOnFailureListener(e -> Log.e(TAG, "Error getting preferences", e));
     }
 
     public static void getAllUsers(OnSuccessListener<QuerySnapshot> listener) {
-        db.collection("users").get().addOnSuccessListener(listener);
+        db.collection("users").get()
+                .addOnSuccessListener(listener)
+                .addOnFailureListener(e -> Log.e(TAG, "Error getting all users", e));
     }
 
     public static void getAllPreferences(OnSuccessListener<QuerySnapshot> listener) {
-        db.collection("preferences").get().addOnSuccessListener(listener);
+        db.collection("preferences").get()
+                .addOnSuccessListener(listener)
+                .addOnFailureListener(e -> Log.e(TAG, "Error getting all preferences", e));
     }
 }
